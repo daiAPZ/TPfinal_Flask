5476554db25d271279c9924dd84d0e0e4a56af91
daiana <azdelugano001mela@gmail.com>
2023-10-04 12:20:21 -0300

    en proceso paso 4 - 1.2

--- cuevana4/movies.py
+++ cuevana4/movies.py
@@ -4,18 +4,16 @@ from werkzeug.exceptions import abort
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
-bp = Blueprint("blog", __name__)
+bp = Blueprint("movies", __name__)
 
 
 @bp.route("/")
 def index():
     db = get_db()
-    posts = db.execute(
-        "SELECT p.id, title, body, created, author_id, username"
-        " FROM post p JOIN user u ON p.author_id = u.id"
-        " ORDER BY created DESC"
+    movies = db.execute(
+        "SELECT title, description, release_year FROM film ORDER BY title ASC;"
     ).fetchall()
-    return render_template("blog/index.html", posts=posts)
+    return render_template("blog/index.html", movies=movies)
 
     # def get_post(id, check_author=True):
     #    post = (
